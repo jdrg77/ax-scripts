3af575a5cb80acaf69ccebd63947a4d4c7c1a1b2
jdrg77 <jdrg77@users.noreply.github.com>
2026-04-19 19:40:31 -0300

    rename MC Highlight: remove version from filename, add update/download URLs

--- Axiom MC Highlight.user.js
+++ Axiom MC Highlight.user.js
@@ -5,8 +5,8 @@
 // @match        *://axiom.trade/*
 // @grant        none
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/jdrg77/ax-scripts/main/Axiom%20MC%20Highlight-1.1.user.js
-// @downloadURL  https://raw.githubusercontent.com/jdrg77/ax-scripts/main/Axiom%20MC%20Highlight-1.1.user.js
+// @updateURL    https://raw.githubusercontent.com/jdrg77/ax-scripts/main/Axiom%20MC%20Highlight.user.js
+// @downloadURL  https://raw.githubusercontent.com/jdrg77/ax-scripts/main/Axiom%20MC%20Highlight.user.js
 // ==/UserScript==
 
 (function () {
