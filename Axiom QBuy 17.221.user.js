229ec986c75be59fcc70ae2eda325e6db40f747d
jdrg77 <jdrg77@users.noreply.github.com>
2026-04-20 18:05:54 -0300

    bump version to 7.6.2

--- Axiom QBuy 17.221.user.js
+++ Axiom QBuy 17.221.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Axiom QBuy 17.221
 // @namespace    http://tampermonkey.net/
-// @version      7.6.1
+// @version      7.6.2
 // @match        https://axiom.trade/*
 // @grant        none
 // @run-at       document-idle
