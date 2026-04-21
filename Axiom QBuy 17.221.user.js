2359d3759bbd607175c42947d385a80f65f86ee1
jdrg77 <jdrg77@users.noreply.github.com>
2026-04-21 02:34:49 -0300

    v8.5: bump version string

--- Axiom QBuy 17.221.user.js
+++ Axiom QBuy 17.221.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Axiom QBuy 17.221
 // @namespace    http://tampermonkey.net/
-// @version      8.4
+// @version      8.5
 // @match        https://axiom.trade/*
 // @grant        none
 // @run-at       document-idle
@@ -1167,6 +1167,7 @@
   setInterval(() => { checkTopPulseReference(); }, 500);
 
 })();
+
 
 
 
