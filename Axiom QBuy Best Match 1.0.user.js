c8e1a2ee244481c0f2518adcaa2a6628ceb9a014
jdrg77 <jdrg77@users.noreply.github.com>
2026-04-22 22:58:31 -0300

    v8.0

--- Axiom QBuy Best Match 1.0.user.js
+++ Axiom QBuy Best Match 1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Axiom QBuy Best Match
 // @namespace    http://tampermonkey.net/
-// @version      7.993
+// @version      8.0
 // @match        https://axiom.trade/*
 // @grant        none
 // @run-at       document-idle
